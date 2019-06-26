a5c4d8affcf8252654cf3010c0c7ea21e68c3150
zhu-1-190426 <913798225@qq.com>
2019-06-26 18:44:50 +0800

    list,if 嵌套

---
+++ 2019-6-26_25list.py
@@ -0,0 +1,18 @@
+'''
+list 翻译就是序列，用来处理一些 有序项目的列表。就像手机的通讯录，待办事项，等等。
+'''
+
+for a in range(5):
+    print(a)
+
+'''for i in range(1,10):
+    print(int(i=i+1))
+'''
+#30 字符串的索引和切片
+a = 'niwenwo ainiyouduoshen'
+print(a)
+newa = '-'.join(a)
+print(newa)
+for a in a:
+    print(a)
+print(newa[4:10])
