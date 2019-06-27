adf34ac18c0369ea25b3c95bf1dc37fbb6fe4bdb
zhu-1-190426 <913798225@qq.com>
2019-06-27 19:26:14 +0800

    split

---
+++ 2019-6-27_28字符串的分割.py
@@ -0,0 +1,8 @@
+'''
+字符串和list之间有很多不得不说的事。比如有同学想要用python去自动抓取某个网页上的下载链接，那就需要对网页的代码进行处理。处理的过程中，免不了要在字符串和list之间进行很多操作。
+'''
+sentence = ' you can do best , because , good good study , day day up '
+print(sentence.split())
+'''
+join则是把一个list中的所有字符串连接成一个字符串。
+'''
