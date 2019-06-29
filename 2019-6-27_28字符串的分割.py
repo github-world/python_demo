2e4789cf776d59db4cee67685abfa115a8947b16
zhu-1-190426 <913798225@qq.com>
2019-06-29 16:55:35 +0800

    2019年6月29日16:55:30

--- 2019-6-27_28字符串的分割.py
+++ 2019-6-27_28字符串的分割.py
@@ -6,3 +6,47 @@ print(sentence.split())
 '''
 join则是把一个list中的所有字符串连接成一个字符串。
 '''
+list1=['google','baidu','sougou','yaoohu'];
+list2=[1,2,3,4,5];
+list3=['a','b','c','d'];
+#访问列表中的值
+print('list1[0]:',list1[0])
+print(list2[2:4])
+#更新list1里面的第三个值为 sina
+print('list1里面第三个值为:',list1[2])
+list1[2]='sina'
+print('list1更新后第三个值为：',list1[2])
+#删除最后一条信息
+print('list1最后一条信息为:',list1)
+del list1[3]
+print('list1最后一条信息已删除',list1)
+#python列表脚本操作符
+print(['hi']*4)
+a=[1,2,3,4,5]
+b=['a','b','c']
+print(len(a),len(b))
+print(a+b)
+print(4*[a])
+print('b' in b)
+for x in [1, 2, 3]: print(x, end=" ")
+for x in [1,2,4,6,8]:print(x,end='')
+##输出science end=参数不设置，默认为末尾换行/n，end=''末尾为空所以不换行
+#嵌套列表
+a=[1,2,3,4]
+b=[5,6,7,8]
+c=[a,b]
+print ([a+b])
+print(c)
+print(c[0])
+print(c[1][0])
+#字符串的索引和切片
+var= 'goodlike'
+for a in var:
+    print(a)
+
+var= 'goodlike'
+for a in var:
+    print(a)
+print('''\\\\\\\\'\'''')
+print (var[0])
+print(var[-1])
---
+++ 2019-6-28_190628.py
@@ -0,0 +1,3 @@
+#字符串的索引和切片
+sentence='i hava a  world char.'
+print(sentence.split(''))
